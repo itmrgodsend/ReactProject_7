ee0cb55ba371b6f99fe7b82b40ab82fda17a1460
itmrgodsend <itmrgodsend@gmail.com>
2020-05-05 00:08:45 +0300

    Stage_2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import TodoList from "./Components/TodoList";
 
 
 function App() {
-  return (
-    <div className='wrapper'>
-      <h1>React</h1>
+    const q = React.useState([
+        {id: 1, completed: false, title: 'Купить хлеб'},
+        {id: 2, completed: false, title: 'Купить масло'},
+        {id: 3, completed: false, title: 'Купить молоко'},
+    ])
 
-        <TodoList />
-    </div>
-  );
+    function toggleTodo(id) {
+        todos = todos.map (todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        })
+    }
+
+    return (
+        <div className='wrapper'>
+            <h1>React</h1>
+
+            <TodoList todos={todos} onToggle={toggleTodo}/>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-
+import PropTypes from 'prop-types'
 
 const styles = {
     ul: {
@@ -10,16 +10,26 @@ const styles = {
     }
 }
 
-const TodoList = () => {
+const TodoList = (props) => {
     return (
         <ul style={styles.ul}>
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
+            {props.todos.map((todo, index) => {
+                return (
+                    <TodoItem
+                        todo={todo}
+                        key={todo.id}
+                        index={index}
+                        onChange={props.onToggle}/>
+                )
+            })}
         </ul>
 
     )
 }
 
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+}
 
 export default TodoList;
