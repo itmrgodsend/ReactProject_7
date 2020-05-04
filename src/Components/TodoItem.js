8086bba2796ea5efb91bbe0ead44a990241d390b
itmrgodsend <itmrgodsend@gmail.com>
2020-05-04 19:56:59 +0300

    Stage_1

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const TodoItem = () => {
+    return (
+    <li>todo</li>
+    )
+}
+
+
+export default TodoItem;
---
+++ src/Components/TodoList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+const TodoList = () => {
+    return (
+        <ul style={styles.ul}>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+        </ul>
+
+    )
+}
+
+
+export default TodoList;
