2f2a4deb9db3756c4ef3e2d0ad70a82a667b85e1
itmrgodsend <itmrgodsend@gmail.com>
2020-05-15 00:08:38 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import TodoList from "./Components/TodoList";
+import Context from "./context";
+import AddTodo from "./Components/AddTodo";
 
 
 function App() {
-    const q = React.useState([
+    const [todos, setTodos] = React.useState([
         {id: 1, completed: false, title: 'Купить белый хлеб'},
         {id: 2, completed: false, title: 'Купить много масло'},
         {id: 3, completed: false, title: 'Купить many молоко'},
     ])
 
     function toggleTodo(id) {
-        todos = todos.map (todo => {
+        setTodos(todos.map(todo => {
             if (todo.id === id) {
                 todo.completed = !todo.completed
             }
             return todo;
-        })
+        }))
     }
 
-    return (
-        <div className='wrapper'>
-            <h1>React</h1>
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id))
 
-            <TodoList todos={todos} onToggle={toggleTodo}/>
-        </div>
+    }
+
+    function addTodo(title) {
+        setTodos(todos.concat([{
+            title,
+            id: Date.now(),
+            completed: false
+        }]))
+
+    }
+    return (
+        <Context.Provider value={{ removeTodo }}>
+            <div className='wrapper'>
+                <h1>React</h1>
+                <AddTodo onCreate={addTodo}/>
+                {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos</p>}
+
+            </div>
+        </Context.Provider>
     );
 }
 
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from 'prop-types'
+import Context from "../context";
 
 const styles = {
     li: {
@@ -17,19 +18,26 @@ const styles = {
 }
 
 const TodoItem = ({todo, index, onChange}) => {
-    console.log('todo', todo)
+    const {removeTodo} = useContext(Context)
+    const classes = []
+
+    if (todo.completed) {
+        classes.push('done');
+    }
     return (
 
         <li style={styles.li}>
-            <span>
-                <input type='checkbox' style={styles.input}
+            <span className={classes.join(' ')}>
+                <input type='checkbox'
+                       checked={todo.completed}
+                       style={styles.input}
                        onChange={() => onChange(todo.id)}/>
                 <strong>{index + 1}</strong>
                 &nbsp;
                 {todo.title}
             </span>
 
-            <button className='rm'>&times;</button>
+            <button className='rm' onClick={removeTodo.bind(null, todo.id)}>&times;</button>
 
 
         </li>
